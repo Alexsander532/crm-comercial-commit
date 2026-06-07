8d951c8c8815f973195975d6dd5f356390e8abe0
Alexsander532 <alexsanderaugusto142019@gmail.com>
2026-06-07 10:44:16 -0300

    feat(users): add user management page with create form
    - UserController: GET /api/users (DIRETOR only, paginated)
    - UsersPage: create user form + user list table
    - Navbar updated with Usuários link
    - Users created: func.aquisicao@commit.com, func.prospeccao@commit.com

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import LeadDetailPage from './pages/LeadDetailPage';
 import LeadFormPage from './pages/LeadFormPage';
 import TasksPage from './pages/TasksPage';
 import KanbanPage from './pages/KanbanPage';
+import UsersPage from './pages/UsersPage';
 import Layout from './components/Layout';
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -16,39 +17,16 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/dashboard"
-            element={<ProtectedRoute><Layout><DashboardPage /></Layout></ProtectedRoute>}
-          />
-          <Route
-            path="/leads"
-            element={<ProtectedRoute><Layout><LeadsPage /></Layout></ProtectedRoute>}
-          />
-          <Route
-            path="/leads/new"
-            element={<ProtectedRoute><Layout><LeadFormPage /></Layout></ProtectedRoute>}
-          />
-          <Route
-            path="/leads/:id"
-            element={<ProtectedRoute><Layout><LeadDetailPage /></Layout></ProtectedRoute>}
-          />
-          <Route
-            path="/leads/:id/edit"
-            element={<ProtectedRoute><Layout><LeadFormPage /></Layout></ProtectedRoute>}
-          />
-          <Route
-            path="/tasks"
-            element={<ProtectedRoute><Layout><TasksPage /></Layout></ProtectedRoute>}
-          />
-          <Route
-            path="/kanban"
-            element={<ProtectedRoute><Layout><KanbanPage /></Layout></ProtectedRoute>}
-          />
+          <Route path="/dashboard" element={<ProtectedRoute><Layout><DashboardPage /></Layout></ProtectedRoute>} />
+          <Route path="/leads" element={<ProtectedRoute><Layout><LeadsPage /></Layout></ProtectedRoute>} />
+          <Route path="/leads/new" element={<ProtectedRoute><Layout><LeadFormPage /></Layout></ProtectedRoute>} />
+          <Route path="/leads/:id" element={<ProtectedRoute><Layout><LeadDetailPage /></Layout></ProtectedRoute>} />
+          <Route path="/leads/:id/edit" element={<ProtectedRoute><Layout><LeadFormPage /></Layout></ProtectedRoute>} />
+          <Route path="/tasks" element={<ProtectedRoute><Layout><TasksPage /></Layout></ProtectedRoute>} />
+          <Route path="/kanban" element={<ProtectedRoute><Layout><KanbanPage /></Layout></ProtectedRoute>} />
+          <Route path="/users" element={<ProtectedRoute><Layout><UsersPage /></Layout></ProtectedRoute>} />
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route
-            path="*"
-            element={<ProtectedRoute><Layout><Navigate to="/dashboard" replace /></Layout></ProtectedRoute>}
-          />
+          <Route path="*" element={<ProtectedRoute><Layout><Navigate to="/dashboard" replace /></Layout></ProtectedRoute>} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
